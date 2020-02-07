6ea00df110a70a89edff31145dfc3fb2ebe03981
Joshnee Kim Cunanan <joshnee.kim.cunanan@gmail.com>
2020-02-07 22:13:46 +0800

    Added implementation for displaying patient data in check up view from firebase

--- app/src/main/java/com/example/dentalprofileapp/profile/view/DentistCheckUpActivity.java
+++ app/src/main/java/com/example/dentalprofileapp/profile/view/DentistCheckUpActivity.java
@@ -2,12 +2,18 @@ package com.example.dentalprofileapp.profile.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 import com.example.dentalprofileapp.R;
 import com.example.dentalprofileapp.databinding.ActivityDentistCheckUpBinding;
+import com.example.dentalprofileapp.profile.entities.Comorbidity;
+import com.example.dentalprofileapp.profile.entities.Patient;
+import com.example.dentalprofileapp.profile.entities.PatientDentalImages;
 import com.example.dentalprofileapp.profile.viewmodel.DentistCheckUpViewModel;
+
+import java.util.ArrayList;
 
 public class DentistCheckUpActivity extends AppCompatActivity {
 
@@ -23,5 +29,33 @@ public class DentistCheckUpActivity extends AppCompatActivity {
         dentistCheckUpBinding.setLifecycleOwner(this);
         dentistCheckUpViewModel = ViewModelProviders.of(this).get(DentistCheckUpViewModel.class);
         dentistCheckUpBinding.setViewmodel(dentistCheckUpViewModel);
+
+        final Bundle extras = getIntent().getExtras();
+        String patientId = extras.getString("patientId");
+        dentistCheckUpViewModel.getPatientData(patientId);
+
+        dentistCheckUpViewModel.getPatientMutableLiveData()
+                .observe(this, new Observer<Patient>() {
+            @Override
+            public void onChanged(Patient patient) {
+                dentistCheckUpViewModel.populatePatientDataToView(patient);
+            }
+        });
+
+        dentistCheckUpViewModel.getComorbidityMutableLiveData()
+                .observe(this, new Observer<ArrayList<Comorbidity>>() {
+            @Override
+            public void onChanged(ArrayList<Comorbidity> comorbidities) {
+                dentistCheckUpViewModel.populateComorbidityDataToView(comorbidities);
+            }
+        });
+
+        dentistCheckUpViewModel.getPatientDentalImagesMutableLiveData()
+                .observe(this, new Observer<PatientDentalImages>() {
+            @Override
+            public void onChanged(PatientDentalImages patientDentalImages) {
+                dentistCheckUpViewModel.populateDentalImagesToView(patientDentalImages);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/dentalprofileapp/profile/viewmodel/DentistCheckUpViewModel.java
+++ app/src/main/java/com/example/dentalprofileapp/profile/viewmodel/DentistCheckUpViewModel.java
@@ -1,16 +1,22 @@
 package com.example.dentalprofileapp.profile.viewmodel;
 
 import android.app.Application;
+import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.dentalprofileapp.profile.entities.Comorbidity;
+import com.example.dentalprofileapp.profile.entities.Patient;
+import com.example.dentalprofileapp.profile.entities.PatientDentalImages;
 import com.example.dentalprofileapp.profile.repository.ComorbidityRepository;
 import com.example.dentalprofileapp.profile.repository.PatientDentalImagesRepository;
 import com.example.dentalprofileapp.profile.repository.PatientRepository;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 public class DentistCheckUpViewModel extends AndroidViewModel {
 
@@ -21,15 +27,107 @@ public class DentistCheckUpViewModel extends AndroidViewModel {
     private FirebaseAuth firebaseAuth;
 
     //patient attributes
-    private MutableLiveData<String> patientName;
-    private MutableLiveData<String> patientId;
+    public MutableLiveData<String> patientName;
+    public MutableLiveData<String> patientId;
+    public MutableLiveData<String> age;
+    public MutableLiveData<String> sex;
+    public MutableLiveData<String> barangay;
+    public MutableLiveData<String> purok;
+    public MutableLiveData<String> pregnant;
+    public MutableLiveData<String> allergies;
+    public MutableLiveData<String> comorbiditiesAttribute;
+    public MutableLiveData<String> urlUpperOcclusal;
+    public MutableLiveData<String> urlLowerOcclusal;
+    public MutableLiveData<String> urlLeftBuccal;
+    public MutableLiveData<String> urlRightBuccal;
+    public MutableLiveData<String> urlFront;
+    public MutableLiveData<String> urlFrontFace;
 
+    private MutableLiveData<Patient> patientMutableLiveData;
+    private MutableLiveData<ArrayList<Comorbidity>> comorbidityMutableLiveData;
+    private MutableLiveData<PatientDentalImages> patientDentalImagesMutableLiveData;
 
     public DentistCheckUpViewModel(@NonNull Application application) {
         super(application);
         firebaseAuth = FirebaseAuth.getInstance();
         patientRepository = new PatientRepository(application);
+        comorbidityRepository = new ComorbidityRepository(application);
+        patientDentalImagesRepository = new PatientDentalImagesRepository(application);
+
+        //patient data
+        patientName = new MutableLiveData<>();
+        patientId = new MutableLiveData<>();
+        age = new MutableLiveData<>();
+        sex = new MutableLiveData<>();
+        barangay = new MutableLiveData<>();
+        purok = new MutableLiveData<>();
+        pregnant = new MutableLiveData<>();
+        allergies = new MutableLiveData<>();
+        //comorbidity data
+        comorbiditiesAttribute = new MutableLiveData<>();
+        //dental images data
+        urlUpperOcclusal = new MutableLiveData<>();
+        urlLowerOcclusal = new MutableLiveData<>();
+        urlLeftBuccal = new MutableLiveData<>();
+        urlRightBuccal = new MutableLiveData<>();
+        urlFront = new MutableLiveData<>();
+        urlFrontFace = new MutableLiveData<>();
+
+        patientMutableLiveData = new MutableLiveData<>();
+        comorbidityMutableLiveData = new MutableLiveData<>();
+        patientDentalImagesMutableLiveData = new MutableLiveData<>();
     }
 
+    public MutableLiveData<Patient> getPatientMutableLiveData() {
+        return patientMutableLiveData;
+    }
 
+    public MutableLiveData<ArrayList<Comorbidity>> getComorbidityMutableLiveData() {
+        return comorbidityMutableLiveData;
+    }
+
+    public MutableLiveData<PatientDentalImages> getPatientDentalImagesMutableLiveData() {
+        return patientDentalImagesMutableLiveData;
+    }
+
+    public void getPatientData(String patientId) {
+        int intPatientId = Integer.parseInt(patientId);
+        patientRepository.getPatientByPatientId(intPatientId, patientMutableLiveData);
+        comorbidityRepository.getComorbiditiesByPatientId(intPatientId, comorbidityMutableLiveData);
+        patientDentalImagesRepository.getPatientDentalImagesByPatientId(intPatientId, patientDentalImagesMutableLiveData);
+    }
+
+    public void populatePatientDataToView(Patient patient) {
+        patientName.setValue(patient.getPatientName());
+        patientId.setValue(Integer.toString(patient.getPatientId()));
+        age.setValue(patient.getAge());
+        sex.setValue(patient.getSex());
+        barangay.setValue(patient.getBarangay());
+        purok.setValue(patient.getPurok());
+        allergies.setValue(patient.getAllergies());
+
+        if(patient.getPregnant()) {
+            pregnant.setValue("Yes");
+        } else {
+            pregnant.setValue("No");
+        }
+    }
+
+    public void populateComorbidityDataToView(ArrayList<Comorbidity> comorbidities) {
+        ArrayList<String> comorbidityNameList = new ArrayList<>();
+        for(Comorbidity comorbidity : comorbidities) {
+            comorbidityNameList.add(comorbidity.getComorbidityName());
+        }
+
+        comorbiditiesAttribute.setValue(TextUtils.join(",", comorbidityNameList));
+    }
+
+    public void populateDentalImagesToView(PatientDentalImages patientDentalImages) {
+        urlUpperOcclusal.setValue(patientDentalImages.getUrlUpperOcclusal());
+        urlLowerOcclusal.setValue(patientDentalImages.getUrlLowerOcclusal());
+        urlLeftBuccal.setValue(patientDentalImages.getUrlLeftBuccal());
+        urlRightBuccal.setValue(patientDentalImages.getUrlRightBuccal());
+        urlFront.setValue(patientDentalImages.getUrlFront());
+        urlFrontFace.setValue(patientDentalImages.getUrlFrontFace());
+    }
 }
